80c086e5605b21c0d5af233bf340ab2595ca065c
karthik <karcse17@gmail.com>
2023-07-21 00:19:12 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,7 +17,7 @@ variable "az_count" {
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
-  default     = "bradfordhamilton/crystal_blockchain:latest"
+  default     = "660656678331.dkr.ecr.us-east-1.amazonaws.com/node:latest"
 }
 
 variable "app_port" {
